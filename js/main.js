1afbaa4d4c1e48b2d8e7001127684bbbfe5f3bb4
Agnieszka Gielata <agielat@o2.pl>
2020-06-18 15:14:49 +0200

    Rozpoczęcie projektu

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let name = 'firstNumber';
+let number = '1';
+console.log(name);
+console.log(number);
+let name2 = 'secondNumber';
+let number2 = '2';
+console.log(name2);
+console.log(number2);
+
+function suma(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+console.log( suma(1, 2) );
